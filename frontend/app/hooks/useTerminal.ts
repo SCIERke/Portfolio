815778543a6d894828384dd5f178c84a18bd44ac
SCIERke <peerapatsetsuk15@gmail.com>
2025-12-18 14:28:06 +0700

    feat: implement terminal interface with command handling, typewriter effect, and initial assets.

---
+++ frontend/app/hooks/useTerminal.ts
@@ -0,0 +1,86 @@
+'use client'
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+interface TerminalHistoryItem {
+  command: string;
+  timestamp: Date;
+  output?: string;
+}
+
+export default function useTerminal(onInfo?: () => void) {
+  const [terminalHistory, setTerminalHistory] = useState<TerminalHistoryItem[]>([]);
+  const chatRef = useRef<HTMLDivElement>(null);
+  
+
+  const handleSubmit = useCallback((value: string) => {
+    console.log("Terminal command:", value);
+
+    // Add command to history
+    const newEntry: TerminalHistoryItem = {
+      command: value,
+      timestamp: new Date(),
+    };
+
+    // Process commands
+    if (value === "clear") {
+      setTerminalHistory([]);
+      return;
+    }
+
+    if (value === "help") {
+      newEntry.output = `
+Usage: <command>
+
+Available Commands:
+clear       Clear terminal history
+help        Show this help message
+info        Open information window
+
+`;
+    } else if (value === "start") {
+      const welcomeText = `=============================================
+==   #   #  #####  #      #       ###      ==
+==   #   #  #      #      #      #   #     ==
+==   #####  #####  #      #      #   #     ==
+==   #   #  #      #      #      #   #     ==
+==   #   #  #####  #####  #####   ###      ==
+=============================================
+==   Welcome new visitor to my website!    ==
+==   Type 'help' for available commands.   ==
+=============================================
+`;
+      newEntry.output = welcomeText;
+
+    } else if (value === "info") {
+      newEntry.output = "Opening info window...";
+      if (onInfo) onInfo();
+    } else {
+      newEntry.output = `There is no '${value}' command`;
+    }
+
+    setTerminalHistory(prev => [...prev, newEntry]);
+  }, [onInfo]);
+
+  useEffect(() => {
+    setTimeout(()=>{
+      handleSubmit("start");
+    }, 500)
+  }, []);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [terminalHistory]);
+
+  const clearHistory = useCallback(() => {
+    setTerminalHistory([]);
+  }, []);
+
+  return {
+    terminalHistory,
+    handleSubmit,
+    clearHistory,
+    chatRef,
+  };
+}
